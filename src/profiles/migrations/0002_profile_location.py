e9c62b63e3f15a89198377348636529da3d11978
Peter Shao <plshao@gmail.com>
2015-10-06 02:41:03 -0700

    src commit

---
+++ src/profiles/migrations/0002_profile_location.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profiles', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='profile',
+            name='location',
+            field=models.CharField(default=b'my location', max_length=1200),
+        ),
+    ]
---
+++ src/profiles/migrations/0005_auto_20150921_0042.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profiles', '0004_auto_20150921_0042'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='profile',
+            name='job',
+        ),
+        migrations.RemoveField(
+            model_name='profile',
+            name='location',
+        ),
+    ]
